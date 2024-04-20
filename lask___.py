384afcb5e18443d72ed7717ac3fd2286299298ca
Lokaaaaa <vlad1020@mail.ru>
2024-04-20 16:43:41 +0300

    Добавил сайтик и иконки некоторые

--- lask___.py
+++ lask___.py
@@ -24,9 +24,9 @@ login_manager.init_app(app)
 
 
 class Users(UserMixin, db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	username = db.Column(db.String(250), unique=True, nullable=False)
-	password = db.Column(db.String(250), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(250), unique=True, nullable=False)
+    password = db.Column(db.String(250), nullable=False)
 
 
 db.init_app(app)
@@ -118,24 +118,48 @@ def home():
 	return render_template("index.html")
 
 
-class Customer:
-    def __init__(self, name, loyalty_level=1, bonus_points=0):
-        self.name = name
-        self.loyalty_level = loyalty_level
-        self.bonus_points = bonus_points
+class Loyality:
 
-    def calculate_bonus_points(self, amount_spent):
-        # Рассчитываем бонусные баллы в зависимости от уровня лояльности
-        if self.loyalty_level <= 1:
-            bonus_rate = 0.1
-        elif self.loyalty_level == 2:
-            bonus_rate = 0.15
-        else:
-            bonus_rate = 0.2
+    @property
+    def make_deposit(self, sum, user):
+        user = Users.query.filter_by(
+            username=user).first()
+        user.balance += sum
+        db.session.commit()
 
-        bonus_points = amount_spent * bonus_rate
-        self.bonus_points += bonus_points
-        return bonus_points
+    @property
+    def make_purchase(self, category, price, user,count=0):
+        user = Users.query.filter_by(
+            username=user).first()
+        user.balance -= price
+
+    @property
+    def daily_free_bonus(self,user):
+        user = Users.query.filter_by(
+            username=user).first()
+        user.cubes += 70 if user.count_days % 4 == 0 else 40
+        db.session.commit()
+
+    @property
+    def get_bonus_points(self, amount_spent, type_of_spends, user):
+        user = Users.query.filter_by(
+            username=user).first()
+        # TODO: Доделать еще
+        hold_points_coef = 0
+        if type_of_spends == "5":
+            hold_points_coef = 0 if amount_spent < 23000 else 0.02 if 23000 <= amount_spent < 32000 else 0.03 if 32000 <= amount_spent < 45000 else 0.04 if 45000 <= amount_spent < 58000 else 0.05 if 58000 <= amount_spent < 72000 else 0.06
+
+        if type_of_spends == "1": # Кафе
+            hold_points_coef = 0 if amount_spent < 300 else 0.03 if 300 <= amount_spent < 1000 else 0.04 if 1000 <= amount_spent < 1600 else 0.05 if 1600 <= amount_spent < 2300 else 0.06 if 2300 <= amount_spent < 3400 else 0.07
+        if type_of_spends == "2":
+            hold_points_coef = 0 if amount_spent < 500 else 0.03 if 500 <= amount_spent < 1400 else 0.04 if 1400 <= amount_spent < 2000 else 0.05
+        if type_of_spends == "3":
+            hold_points_coef = 0 if amount_spent < 500 else 0.03 if 500 <= amount_spent < 1200 else 0.04 if 1200 <= amount_spent < 2000 else 0.05
+        if type_of_spends == "4":
+            hold_points_coef = 0 if amount_spent < 3000 else 0.04 if 3000 <= amount_spent < 5000 else 0.05 if 5000 <= amount_spent < 12000 else 0.07
+
+        user.cubes += amount_spent * hold_points_coef
+        db.session.commit()
 
     def spend_bonus_points(self, points_spent):
         if points_spent <= self.bonus_points:
@@ -144,19 +168,20 @@ class Customer:
         else:
             return False
 
+
 # Создаем экземпляр клиента
-customer1 = Customer("Alice")
+customer1 = Loyality()
 
 # Совершаем покупки (поход в кино)
 amount_spent = 50
-customer1.calculate_bonus_points(amount_spent)
+# customer1.get_bonus_points(amount_spent)
 
 # Бронируем отель
 hotel_booking_cost = 200
-customer1.calculate_bonus_points(hotel_booking_cost)
+# customer1.get_bonus_points(hotel_booking_cost)
 
 # Проверяем баланс бонусных баллов
-print(f"{customer1.name} имеет {customer1.bonus_points} бонусных баллов.")
+# print(f"{customer1.name} имеет {customer1.bonus_points} бонусных баллов.")
 
 
 if __name__ == '__main__':
